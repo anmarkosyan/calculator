1b5b25e6c057c879065f2d78cad9c7a051df60fb
anmarkosyan <anush.markos@gmail.com>
2020-05-28 18:07:55 +0400

    calculator project

---
+++ JavaScript_calculator/main.js
@@ -0,0 +1,35 @@
+//alert('hi there')
+
+//store 4 variables
+let prevVal = '';
+let nextVal= '';
+let resultVal = '';
+let decimalClicked = false;
+let valMemStore = '';
+
+//functions
+
+function numButPress(num){
+
+}
+function mathButPress(operator){
+
+}
+function equalButPress(){
+
+}
+function clearButPress(){
+     prevVal = '';
+     nextVal= '';
+     resultVal = '';
+     decimalClicked = false;
+     document.getElementById('entry').value = '0';
+}
+function copyButPress(){
+    valMemStore = document.getElementById('entry').value;
+
+}
+function pasteButPress(){
+
+}
+
