3d0f80cdb3b4a5ccd1ad4690a8e4676f01b1e740
anmarkosyan <anush.markos@gmail.com>
2020-05-29 16:57:45 +0400

    calculator project

--- JavaScript_calculator/main.js
+++ JavaScript_calculator/main.js
@@ -2,34 +2,84 @@
 
 //store 4 variables
 let prevVal = '';
-let nextVal= '';
+let newVal= '';
 let resultVal = '';
+let mathOperator = '';
 let decimalClicked = false;
 let valMemStore = '';
 
 //functions
-
 function numButPress(num){
+     if(resultVal){
+          newVal = num;
+          resultVal = '';
+     }else{
+          if(num === '.'){
+               if(decimalClicked !== true){
+                    newVal += num;
+                    decimalClicked = true;
+               }
+          }else{
+               newVal += num;
+          }
+     }
+     document.getElementById('entry').value = newVal;
+
 
 }
 function mathButPress(operator){
+     if(!resultVal){
+          prevVal = newVal;
+     }else{
+          prevVal = resultVal;
+     }
+     newVal = '';
+     decimalClicked = false;
+     mathOperator = operator;
+     resultVal = '';
+     document.getElementById('entry').value = '';
 
 }
 function equalButPress(){
+     decimalClicked = false;
+     prevVal = parseFloat(prevVal);
+     newVal = parseFloat(newVal);
+
+     switch (mathOperator) {
+          case '+':
+               resultVal = prevVal + newVal;
+               break;
+          case '-':
+               resultVal = prevVal - newVal;
+               break;
+          case '*':
+               resultVal = prevVal * newVal;
+               break;
+          case '/':
+               resultVal = prevVal / newVal;
+               break;
+          default:
+               resultVal = newVal;
+     }
+     prevVal = resultVal;
+     document.getElementById('entry').value = resultVal;
 
 }
 function clearButPress(){
-     prevVal = '';
-     nextVal= '';
-     resultVal = '';
-     decimalClicked = false;
+      prevVal = '';
+      newVal= '';
+      resultVal = '';
+      decimalClicked = false;
      document.getElementById('entry').value = '0';
 }
 function copyButPress(){
-    valMemStore = document.getElementById('entry').value;
+     valMemStore = document.getElementById('entry').value;
 
 }
 function pasteButPress(){
-
+     if(valMemStore) {
+          document.getElementById('entry').value = valMemStore;
+          newVal = valMemStore;
+     }
 }
 
